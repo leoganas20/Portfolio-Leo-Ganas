8d3fb8f2ce5e455abe7f72bc22286937faf5cb89
Leo Ganas <leoganas.jr@gmail.com>
2024-12-14 17:24:50 +0800

    Added Download CV Button Integration

---
+++ Leo.Project.Portfolio.Web/wwwroot/downloadCV.js
@@ -0,0 +1,7 @@
+﻿function downloadCv() {
+    // Create a hidden <a> element and trigger a download
+    const link = document.createElement('a');
+    link.href = '/Leo-CV.pdf';  // Path to your PDF in wwwroot folder
+    link.download = 'Leo-CV.pdf'; // The file name for download
+    link.click();  // Trigger the download
+}
