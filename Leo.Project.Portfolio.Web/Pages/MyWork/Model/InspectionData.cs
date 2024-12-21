b8b231a45570aa28bb00f195354f0378ae0eddaf
Leo Ganas <leoganas.jr@gmail.com>
2024-12-21 17:47:33 +0800

    Added My Work Updates Web View

---
+++ Leo.Project.Portfolio.Web/Pages/MyWork/Model/InspectionData.cs
@@ -0,0 +1,13 @@
+﻿namespace Leo.Project.Portfolio.Web.Pages.MyWork.Model;
+
+public class InspectionData
+{
+    public int AssetId { get; set; }
+    public string AssetType { get; set; }
+    public DateTime InspectionDate { get; set; }
+    public int ConditionRating { get; set; }
+    public string Defects { get; set; }
+    public string InspectionNotes { get; set; }
+    public DateTime NextInspectionDue { get; set; }
+    public string Status { get; set; }
+}
